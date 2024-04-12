abbd9d94be383653084539b51a89eda5d19bd0d4
resse李四 <resse.zhu@flip.fun>
2024-04-12 19:37:50 +0800

    fix: 不能编译的问题

---
+++ yoga/YogaVoid.h
@@ -0,0 +1,16 @@
+//
+//  YogaVoid.h
+//  YogaSwift
+//
+//  Created by resse.zhu on 2024/4/12.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YogaVoid : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ yoga/YogaVoid.m
@@ -0,0 +1,12 @@
+//
+//  YogaVoid.m
+//  YogaSwift
+//
+//  Created by resse.zhu on 2024/4/12.
+//
+
+#import "YogaVoid.h"
+
+@implementation YogaVoid
+
+@end
