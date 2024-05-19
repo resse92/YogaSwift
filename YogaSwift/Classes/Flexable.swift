f5234e7b1c745b825eaea9ced38d0cc0dfd3bd2f
resse <zrl920825@hotmail.com>
2024-05-19 15:21:40 +0800

    fix: name

--- YogaSwift/Classes/Flexable.swift
+++ YogaSwift/Classes/Flexable.swift
@@ -5,16 +5,14 @@
 //  Created by resse on 2020/9/18.
 //
 
-public protocol ViewType: AnyObject {
+public protocol Flexable: AnyObject {
     var flexSpec: FlexSpec { get }
-    var parent: (any ViewType)? { get }
+    var parent: (any Flexable)? { get }
     
     func sizeThatFits(_ size: CGSize) -> CGSize
     
-    func addSubItem(item: any ViewType) -> Bool
+    func addSubItem(item: any Flexable) -> Bool
     func removeFromParent() -> Bool
     
     var frame: CGRect { set get }
-    
-    func didCalculateLayout()
 }
